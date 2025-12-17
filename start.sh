22e94ab1d5e61981b386987adce92feed953bea2
kingstonebridge2026 <jamalassker2032@gmail.com>
2025-12-18 02:03:29 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
+
 #!/bin/bash
 set -e
 
 echo "Creating strategy directory..."
 mkdir -p /freqtrade/user_data/strategies
 
-echo "Downloading strategy repository..."
+echo "Downloading strategy repository (tar)..."
 curl -L \
-https://github.com/iterativv/NostalgiaForInfinity/archive/refs/heads/main.zip \
--o /tmp/nfi.zip
+https://github.com/iterativv/NostalgiaForInfinity/archive/refs/heads/main.tar.gz \
+-o /tmp/nfi.tar.gz
 
-echo "Unzipping..."
-unzip /tmp/nfi.zip -d /tmp
+echo "Extracting strategy..."
+tar -xzf /tmp/nfi.tar.gz -C /tmp
 
 echo "Copying strategy file..."
 cp /tmp/NostalgiaForInfinity-main/NostalgiaForInfinityX7.py \
